381f5da3b590476e504b8077b8ff434d98cd6a08
MAJD729 <salilahmajdeddin@gmail.com>
2020-07-02 03:07:48 +0200

    second

---
+++ jQuery assignment 2/js.js
@@ -0,0 +1,19 @@
+var first = $("#first")
+var second = $("#second")
+var third = $("#third")
+
+first.click(function(){
+
+    first.attr("src", "./1b.jpg")
+
+});
+second.click(function(){
+
+    second.attr("src", "./2b.jpg")
+    
+});
+third.click(function(){
+
+    third.attr("src", "./3b.jpg")
+    
+});
